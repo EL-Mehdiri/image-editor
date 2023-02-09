b7568fef750b9c00f8e77daad2a63a002683de68
MehdiRidmani <mahdiridmani22@gmail.com>
2023-02-09 12:53:05 +0100

    center container

--- main.js
+++ main.js
@@ -52,6 +52,7 @@ upload.onchange = function(){
     }
 }
 
+
 let filters = document.querySelectorAll("ul li input")
 filters.forEach(filter =>{
     filter.addEventListener('input', function(){
@@ -59,7 +60,7 @@ filters.forEach(filter =>{
         `
         saturate(${saturate.value}%)
         contrast(${contrast.value}%)
-        brightness(${brightness.value}%)
+        brightness(${brightness.value}%) 
         sepia(${sepia.value}%)
         grayscale(${grayscale.value})
         blur(${blur.value}px)
